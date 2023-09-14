1d307d38041374ad25b6b27b0b6eaf4877b33d1d
zinaida <zinaida.j84@gmail.com>
2023-09-14 11:01:38 +0200

    Spellcheck renamed Glascube to Glasscube

--- glascube/src/main/java/com/codecool/glasscube/GlasscubeApplication.java
+++ glascube/src/main/java/com/codecool/glasscube/GlasscubeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GlascubeApplication {
+public class GlasscubeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GlascubeApplication.class, args);
+		SpringApplication.run(GlasscubeApplication.class, args);
 	}
 
 }
