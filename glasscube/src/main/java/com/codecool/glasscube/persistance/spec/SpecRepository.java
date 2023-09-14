15a1c030b7e7c6c18d044cd29aab99e596a0fabb
zenais <zinaida-x@hotmail.com>
2023-09-14 20:38:36 +0200

    creates SpecRepository

---
+++ glasscube/src/main/java/com/codecool/glasscube/persistance/spec/SpecRepository.java
@@ -0,0 +1,6 @@
+package com.codecool.glasscube.persistance.spec;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SpecRepository extends JpaRepository<Spec, String> {
+}
