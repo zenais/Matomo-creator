38f01520638515c8bbce275866d690165e0bfe63
zinaida <zinaida.j84@gmail.com>
2023-09-14 10:51:54 +0200

    project skeleton created

---
+++ glascube/src/main/java/com/codecool/glascube/GlascubeApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.glascube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlascubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlascubeApplication.class, args);
+	}
+
+}
---
+++ glascube/src/test/java/com/codecool/glascube/GlascubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.glascube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlascubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
